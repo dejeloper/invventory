41b0997f0fe2cda59c427eaef0a5306b81811625
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-05-29 12:33:13 -0500

    Add lazy loading for images in header toggle theme component

--- src/components/header/header-toggle-theme.tsx
+++ src/components/header/header-toggle-theme.tsx
@@ -1,16 +1,26 @@
 "use client";
+
 import { RiMoonFill, RiSunFill } from "react-icons/ri";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export function HeaderToggleTheme() {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   const changeTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
 
+  if (!mounted) return null;
+
   return (
     <>
-      {theme === "dark" ? (
+      {resolvedTheme === "dark" ? (
         <button
           className="w-8 h-8 flex items-center justify-center bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-500 transition duration-150 rounded-full"
           onClick={changeTheme}
