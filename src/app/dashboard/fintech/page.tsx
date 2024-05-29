adaab996879cce3e3c2136622d9f3ed8924fe41b
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-05-29 02:43:35 -0500

    Add new pages for dashboard, ecommerce, inbox, and settings

---
+++ src/app/dashboard/fintech/page.tsx
@@ -0,0 +1,9 @@
+export default function FintechPage() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <h1 className="text-4xl font-bold text-center text-slate-800 dark:text-slate-200">
+        Fintech Page
+      </h1>
+    </div>
+  );
+}
---
+++ src/app/settings/list/page.tsx
@@ -0,0 +1,9 @@
+export default function ListPage() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <h1 className="text-4xl font-bold text-center text-slate-800 dark:text-slate-200">
+        Settings List Page
+      </h1>
+    </div>
+  );
+}
