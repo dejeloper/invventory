e6808d401a42dd62e82a3e9f38ae420770b1574f
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-05-29 12:39:34 -0500

    Update Sidebar component to use separate files for mobile and desktop views

--- src/components/sidebar/index.ts
+++ src/components/sidebar/index.ts
@@ -3,3 +3,6 @@ export * from "./sidebar-link-group";
 export * from "./sidebar-menu";
 export * from "./sidebar-menu-individual";
 export * from "./sidebar-menu-items";
+export * from "./sidebar-mobile";
+export * from "./sidebar-desktop";
+export * from "./sidebar-logo-wait";
