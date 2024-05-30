9e9ea5d84bfe7fb3ca3d6b2b67d03d6641f9b1f4
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-05-29 20:47:05 -0500

    Update Header component to use Button component for sidebar toggle

--- src/components/sidebar/sidebar-logo-wait.tsx
+++ src/components/sidebar/sidebar-logo-wait.tsx
@@ -3,8 +3,8 @@ import { RiGhostLine } from "react-icons/ri";
 
 export function SidebarLogoWait() {
   return (
-    <div className="flex justify-center">
-      <div className="w-16 h-16 py-4 px-6 text-white bg-slate-800 border-b border-slate-200 dark:bg-slate-800 dark:border-slate-700">
+    <div className="md:flex justify-start md:w-[255px] hidden">
+      <div className="w-full h-16 py-4 px-6 text-white bg-slate-800 border-b border-slate-200 dark:bg-slate-800 dark:border-slate-700">
         <RiGhostLine className="w-8 h-8" />
       </div>
     </div>
